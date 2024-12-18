469de5c3e514b7cc23b4260cb949989f593abe1e
JustSypth <justsypth@gmail.com>
2024-12-18 11:38:59 +0100

    Alpha release: Added support for Windows.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 struct ConfigInit {
     c: String,
     p: String,
-    video: PathBuf
+    video: PathBuf,
 }
 
 fn main() {
@@ -22,20 +22,25 @@ fn main() {
     // Loop through args and check if it's a flag or vid
     while let Some(flag) = args_iter.next() {
         match flag.to_lowercase().as_str() {
-            "-c" | "-p" => {handle_args(&mut config, &flag, args_iter.next() );},
-            _ => {
-                match is_video(flag) {
-                    Some(var) => config.video = var,
-                    _ => {
-                        eprintln!("Error: Invalid path or not a video \"{}\"", flag);
-                        std::process::exit(1);
-                    }
-                }
+            "-c" | "-p" => {
+                handle_args(&mut config, &flag, args_iter.next());
             }
+            _ => match is_video(flag) {
+                Some(var) => config.video = var,
+                _ => {
+                    eprintln!("Error: Invalid path or not a video \"{}\"", flag);
+                    std::process::exit(1);
+                }
+            },
         }
     }
 
-    println!("\nConfig: C: {}, P: {} \nVideo: {}", config.c, config.p, config.video.display());
+    println!(
+        "\nConfig: C: {}, P: {} \nVideo: {}",
+        config.c,
+        config.p,
+        config.video.display()
+    );
 
     // Add "-output" to output filename
     let output_path = config.video.with_file_name(format!(
@@ -44,17 +49,28 @@ fn main() {
         config.video.extension().unwrap().to_str().unwrap()
     ));
 
-    let execute = {
+    let execute_arg = format!(
+        "ffmpeg -i \"{}\" -vcodec libx264 -crf {} -preset {} -acodec aac -b:a 128k -y \"{}\"",
+        &config.video.display(),
+        &config.c,
+        &config.p,
+        &output_path.display()
+    );
+
+    let execute = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/C")
+            .arg(&execute_arg)
+            .output()
+            .expect("Failed to execute ffmpeg.")
+    } else {
         Command::new("sh")
             .arg("-c")
-            .arg(format!(
-                "ffmpeg -i \"{}\" -vcodec libx264 -crf {} -preset {} -acodec aac -b:a 128k -y \"{}\"",
-                &config.video.display(), &config.c, &config.p, &output_path.display()
-            ))
+            .arg(&execute_arg)
             .output()
             .expect("Failed to execute ffmpeg.")
     };
-    
+
     let output = format!(
         "Standard Output:\n{}\nStandard Error:\n{}",
         String::from_utf8_lossy(&execute.stdout),
@@ -66,42 +82,39 @@ fn main() {
 
 fn handle_args(config: &mut ConfigInit, flag: &str, args_iter: Option<&String>) {
     match args_iter {
-
-        Some(value) => {
-            match flag {
-                "-c" => {
-                    if let Ok(num) = value.parse::<u32>() {
-                        if (1..=100).contains(&num) {
-                            config.c = num.to_string();
-                        } else {
-                            eprintln!("Error: Compress value must be between 1-100!");
-                            std::process::exit(1);
-                        }
+        Some(value) => match flag {
+            "-c" => {
+                if let Ok(num) = value.parse::<u32>() {
+                    if (1..=100).contains(&num) {
+                        config.c = num.to_string();
                     } else {
-                        eprintln!("Error: Compress value must be a number (1-100)!");
+                        eprintln!("Error: Compress value must be between 1-100!");
                         std::process::exit(1);
                     }
-                },
-                "-p" => {
-                    let allowed_strings: [&str; 4] = ["ultrafast", "veryfast", "slow", "veryslow"];
-                    if allowed_strings.contains(&value.as_str()) {
-                        config.p = value.to_string();
-                    } else {
-                        eprintln!(
-                            "Error: Invalid preset (-p) \nList of valid presets: {}",
-                            allowed_strings.join(", ")
-                        );
-                        std::process::exit(1);
-                    }
-                },
-                _ => (),
+                } else {
+                    eprintln!("Error: Compress value must be a number (1-100)!");
+                    std::process::exit(1);
+                }
             }
-        }
+            "-p" => {
+                let allowed_strings: [&str; 4] = ["ultrafast", "veryfast", "slow", "veryslow"];
+                if allowed_strings.contains(&value.as_str()) {
+                    config.p = value.to_string();
+                } else {
+                    eprintln!(
+                        "Error: Invalid preset (-p) \nList of valid presets: {}",
+                        allowed_strings.join(", ")
+                    );
+                    std::process::exit(1);
+                }
+            }
+            _ => (),
+        },
 
         None => {
             eprintln!("Missing argument after {}", flag);
             std::process::exit(1);
-        },
+        }
     }
 }
 
